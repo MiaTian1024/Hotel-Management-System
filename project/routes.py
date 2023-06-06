8b9ba53c4a093bce61a79a15c5c17816f76b40c3
MiaTian1024 <sddxtm123@gmail.com>
2023-06-06 17:52:34 +1200

    homepage setup

--- project/routes.py
+++ project/routes.py
@@ -7,7 +7,7 @@ from flask import url_for
 
 @app.route("/")
 def home():
-    return render_template("login.html", title="login")
+    return render_template("home.html", title="login")
 
 @app.route("/dashboard")
 def dashboard():
