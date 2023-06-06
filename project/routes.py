b820e0e6e6831c3183b0a9da5e87485270a8becb
MiaTian1024 <sddxtm123@gmail.com>
2023-06-06 16:28:44 +1200

    initial setup

---
+++ project/routes.py
@@ -0,0 +1,14 @@
+from project import app
+
+from flask import render_template
+from flask import request, jsonify
+from flask import redirect
+from flask import url_for
+
+@app.route("/")
+def home():
+    return render_template("login.html", title="login")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
