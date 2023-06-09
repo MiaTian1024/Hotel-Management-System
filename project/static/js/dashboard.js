2265906f7806995527261dc7ac4bc7ea05f74458
MiaTian1024 <sddxtm123@gmail.com>
2023-06-09 14:01:44 +1200

    add profile dropdown menu

--- project/static/js/dashboard.js
+++ project/static/js/dashboard.js
@@ -6,3 +6,16 @@ sidebarBtn.onclick = function () {
     sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
   } else sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
 };
+
+
+function profileToggle(){
+    const toggleProfile = document.querySelector('.profile_dd')
+    toggleProfile.classList.toggle('active')
+}
+
+function notificationToggle(){
+    const toggleNotification = document.querySelector('.notification_dd')
+    toggleNotification.classList.toggle('active')
+}
+
+
